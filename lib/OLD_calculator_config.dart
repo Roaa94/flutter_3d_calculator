d3d29c2f411f2ae69a3996f6f5f824e038c61460
roaa94 <roaa94khaddam@gmail.com>
2022-11-11 21:30:13 +0300

    feat: additional calculator implementation using a single CustomPainter for all keyes - TERRIBLE PERFORMANCE

---
+++ lib/OLD_calculator_config.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CalculatorConfig {
+  const CalculatorConfig({
+    required this.size,
+    required this.fontSize,
+    required this.keysGap,
+  });
+
+  final Size size;
+  final double fontSize;
+  final double keysGap;
+}
